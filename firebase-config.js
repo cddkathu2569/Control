a5c9de2e9bfb5ff9db87b2eeed9a71cb05a486dc
cddkathu2569 <cddkathu69@gmail.com>
2026-06-23 16:35:29 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+// ตั้งค่าโดย Firebase Auto Setup — 23/6/2569
+const firebaseConfig = {
+  apiKey:            "AIzaSyDbg7YWQeH5n5wfRSaaFZHDUzQp6wiGyZ0",
+  authDomain:        "thairat-register.firebaseapp.com",
+  projectId:         "thairat-register",
+  storageBucket:     "thairat-register.firebasestorage.app",
+  messagingSenderId: "699034353162",
+  appId:             "1:699034353162:web:4de198b2a916b0be72e9f3"
+};
